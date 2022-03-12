6e42965bd91924c9d48f0b14de9f4035b4eb9d3a
CJM1994 <connorjohnmullin@gmail.com>
2022-03-12 20:47:13 +0000

    Add functionality to add todos, new login field

---
+++ routes/todos.js
@@ -0,0 +1,46 @@
+/*
+ * All routes for Todos are defined here
+ * Since this file is loaded in server.js into api/users,
+ *   these routes are mounted onto /users
+ * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
+ */
+
+const express = require('express');
+const router = express.Router();
+
+module.exports = (db) => {
+
+
+  router.post('/', (req, res) => {
+
+    const values = [req.session.userId, req.body.submit, 'MOVIES'];
+
+    db.query(`
+      INSERT INTO todos (user_id, description, category)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+      `, values)
+      .then(data => {
+        console.log(`ADDED ${JSON.stringify(data.rows)} TO TABLE TODOS}`);
+      });
+  });
+
+  // Get back to this after frontend functional
+  // router.post('/:id/delete', (req, res) => {
+
+  //   if (req.session.userId) {
+  //   const values = [req.session.userId];
+
+  //   db.query(`
+  //   DELETE * FROM todos
+  //   WHERE user_id = $1
+  //   AND id =
+  //   `)
+
+  //   }
+
+  // });
+
+  return router;
+
+};
