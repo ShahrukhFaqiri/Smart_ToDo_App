9ca878dea95e4767105e922ff5922b42039c44c8
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-03-12 06:51:47 +0000

    Add registration page, add edit username page

--- db/schema/02_todos.sql
+++ db/schema/02_todos.sql
@@ -1,9 +1,9 @@
 -- Drop and recreate ToDos table (Example)
-
 DROP TABLE IF EXISTS todos CASCADE;
+
 CREATE TABLE todos (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
-  category VARCHAR(255) DEFAULT NULL;
-  timestamp TIMESTAMP DEFAULT NOW();
+  category VARCHAR(255) DEFAULT NULL,
+  timestamp TIMESTAMP DEFAULT NOW()
 );
