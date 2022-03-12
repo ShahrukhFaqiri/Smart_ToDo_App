5f56eedebef24011d685d61f630e8714c3eb8ee6
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-03-12 04:09:08 +0000

    Added the logout route

--- routes/users.js
+++ routes/users.js
@@ -5,7 +5,6 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const cookieSession = require("cookie-session");
 const express = require("express");
 const router = express.Router();
 
@@ -20,26 +19,29 @@ module.exports = (db) => {
         res.status(500).json({ error: err.message });
       });
   });
-
+  //POST: Login Route.
   router.post("/login", (req, res) => {
-    // console.log(`WE ARE HEREL`,req);
     const body = req.body.submit;
-
     db.query(`SELECT * FROM users WHERE name =$1`, [body])
-      .then(data => {
+      .then((data) => {
         const users = data.rows[0].name;
-        if(users) {
+        if (users) {
           req.session.user = users;
           res.redirect("/");
         }
       })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.stack });
+      .catch((err) => {
+        res.status(500).json({ error: err.stack });
       });
   });
 
+  router.post("/logout", (req, res) => {
+    if (req.session.user) {
+      req.session = null;
+      res.redirect("/");
+    }
+    res.status(403).send(`Hey bud, please login before trying to logout!`)
+  });
 
   return router;
 };
