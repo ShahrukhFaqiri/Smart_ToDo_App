ccce16ed7b02a48febe9ad27902c374859e5fbf4
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-03-12 01:54:07 +0000

    removed unused routes, added login user route

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+
 // PG database client/connection setup
 const { Pool } = require("pg");
 const dbParams = require("./lib/db.js");
@@ -36,13 +37,11 @@ app.use(express.static("public"));
 // Separated Routes for each Resource
 // Note: Feel free to replace the example routes below with your own
 const usersRoutes = require("./routes/users");
-const widgetsRoutes = require("./routes/widgets");
 
 // Mount all resource routes
 // Note: Feel free to replace the example routes below with your own
-app.use("/api/users", usersRoutes(db));
-app.use("/api/widgets", widgetsRoutes(db));
-// Note: mount other resources here, using the same pattern above
+app.use("/users", usersRoutes(db));
+// Note: mounts other resources here, using the same pattern above
 
 // Home page
 // Warning: avoid creating more routes in this file!
