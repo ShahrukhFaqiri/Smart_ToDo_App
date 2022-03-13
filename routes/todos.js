3c5911dab186e7deb9b857072b012fcc0493fe07
Sha Faqiri <shahrukh.faqiri@gmail.com>
2022-03-13 21:40:09 +0000

    Add/Fix user inputs to the categories, integrated movie api

--- routes/todos.js
+++ routes/todos.js
@@ -5,60 +5,17 @@
  * See: https://expressjs.com/en/guide/using-middleware.html#middleware.router
  */
 
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 module.exports = (db) => {
-
-
-  router.post('/', (req, res) => {
-
-    if (req.session.userId) {
-      const values = [req.session.userId, req.body.submit, 'MOVIES'];
-      db.query(`
-        INSERT INTO todos (user_id, description, category)
-        VALUES ($1, $2, $3)
-        RETURNING *;
-        `, values)
-        .then(data => {
-          console.log(`ADDED ${JSON.stringify(data.rows)} TO TABLE TODOS}`);
-        });
-    } else {
-      res.status(403).send(`Please login to make a task!`)
-    }
-    // res.send('hello');
-
-
-  });
-
-
-
-  // Get back to this after frontend functional
-  // router.post('/:id/delete', (req, res) => {
-
-  //   if (req.session.userId) {
-  //   const values = [req.session.userId];
-
-  //   db.query(`
-  //   DELETE * FROM todos
-  //   WHERE user_id = $1
-  //   AND id =
-  //   `)
-
-  //   }
-
-  // });
-
   router.get("/display", (req, res) => {
     db.query(`SELECT * FROM todos;`).then((data) => {
-      data.rows.forEach(row => {
-        console.log(data.rows);
-      })
+      // data.rows.forEach((row) => {
+      //   console.log(data.rows);
+      // });
       res.send(data.rows);
-
-    })
-
-  })
+    });
+  });
   return router;
-
 };
