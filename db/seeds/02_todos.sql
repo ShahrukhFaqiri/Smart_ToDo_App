cc7313e8bad3565b5e92383866d1484cde1237b0
CJM1994 <connorjohnmullin@gmail.com>
2022-03-13 00:29:24 +0000

    Add seed file for todos table for testing purposes

---
+++ db/seeds/02_todos.sql
@@ -0,0 +1,8 @@
+-- Todos table seeds here (Example)
+INSERT INTO todos (user_id, category, description)
+VALUES (4, 'Food', 'Pizza'),
+       (4, 'Movies', 'Shrek'),
+       (4, 'Movies', 'Shrek 2'),
+       (4, 'Movies', 'Shrek the Third'),
+       (4, 'Movies', 'Puss in Boots'),
+       (4, 'Movies', 'Shrek Forever After');
